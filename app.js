d971faa00229f3d9c3ca1ad3c4f1cf5a2384e8c3
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 09:51:20 +0430

    adding some changes

--- app.js
+++ app.js
@@ -10,5 +10,19 @@ const prevBtn = document.querySelector(".prev-btn");
 container.innerHTML = people.map((person, slideIndex) => {
 
     const{img, name, job, text} = person;
-    return ``
+    return `
+        <article class="slide">
+            <img src="https://www.course-api.com/images/people/person-1.jpeg" class="img" alt="jane doe">
+            <h4>jane doe</h4>
+            <p class="title">product designer</p>
+            <p class="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Provident similique quia facere obcaecati tempora 
+            labore exercitationem, nulla ipsam dolorum quo?
+            </p>
+            <div class="quote-icon">
+            <div class="fas fa-quote-right">
+            </div>
+            </div>
+    </article>`
 })
