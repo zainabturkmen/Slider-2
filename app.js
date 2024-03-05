951b29989c3aa4ff6718a098372ccb632374a101
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 09:39:01 +0430

    adding some changes

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // 
+import people from "./data.js";
+
+const container = document.querySelector(".slide-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+
+// set slides
