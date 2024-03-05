62c44898ffcb52acdb41aeb722e1f3ad411197ae
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 09:55:40 +0430

    adding some changes

--- app.js
+++ app.js
@@ -12,17 +12,15 @@ container.innerHTML = people.map((person, slideIndex) => {
     const{img, name, job, text} = person;
     return `
         <article class="slide">
-            <img src="https://www.course-api.com/images/people/person-1.jpeg" class="img" alt="jane doe">
-            <h4>jane doe</h4>
-            <p class="title">product designer</p>
+            <img src="${img}" class="img" alt="jane doe">
+            <h4>${name}</h4>
+            <p class="title">${job}</p>
             <p class="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Provident similique quia facere obcaecati tempora 
-            labore exercitationem, nulla ipsam dolorum quo?
+            ${text}
             </p>
             <div class="quote-icon">
             <div class="fas fa-quote-right">
             </div>
             </div>
     </article>`
-})
+}).join("");
